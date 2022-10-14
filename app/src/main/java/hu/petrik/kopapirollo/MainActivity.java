19b26f57b4e086766d90ff311a5ad3f235c56377
Sárándi <sarandi.balint@gmail.com>
2022-10-14 08:37:24 +0200

    Layout & id & init method

--- app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
+++ app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
@@ -3,12 +3,30 @@ package hu.petrik.kopapirollo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private Button ko, papir, ollo;
+    private ImageView enDobas, robotDobas;
+    private TextView eredmeny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        ko = findViewById(R.id.ko);
+        papir = findViewById(R.id.papir);
+        ollo = findViewById(R.id.ollo);
+        enDobas = findViewById(R.id.enDobas);
+        robotDobas = findViewById(R.id.robotDobas);
+        eredmeny = findViewById(R.id.eredmeny);
     }
 }
